04240384b33ea0de9dc20bff0137a2e627222467
Alex <format255@bk.ru>
2017-07-07 22:30:23 +0300

    2

--- src/actions/product.js
+++ src/actions/product.js
@@ -6,3 +6,34 @@ function add(id) {
       payload: id
    };
 }
+
+function compare(id) {
+   return {
+      type: constants.COMPARE,
+      payload: id
+   };
+}
+
+function order(id) {
+   return {
+      type: constants.ORDER,
+      payload: id
+   };
+}
+
+function increase(val) {
+   return {
+      type: constants.INCREASE,
+      payload: val
+   };
+}
+
+function decrease(val) {
+   return {
+      type: constants.ORDER,
+      payload: val
+   };
+}
+
+
+export { add, compare, order, increase, decrease };
